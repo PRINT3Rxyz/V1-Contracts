a07e9a996b26607856c88ada75f442328917f395
PRINT3R <brrr@print3r.xyz>
2023-10-24 13:03:38 +0100

    Re-named BRRR to BRRR-LP

--- src/core/BRRR.sol
+++ src/core/BRRR.sol
@@ -5,9 +5,9 @@ pragma solidity 0.8.18;
 import "../tokens/MintableBaseToken.sol";
 
 contract BRRR is MintableBaseToken {
-    constructor() MintableBaseToken("BRRR", "BRRR", 0) {}
+    constructor() MintableBaseToken("BRRR-LP", "BRRR-LP", 0) {}
 
     function id() external pure returns (string memory _name) {
-        return "BRRR";
+        return "BRRR-LP";
     }
 }
